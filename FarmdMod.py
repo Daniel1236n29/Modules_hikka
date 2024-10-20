b000dcd67b5b5279e4c1fc2cbfc7cf9261eb1831
Daniil <140244813+Daniel1236n29@users.noreply.github.com>
2024-10-20 09:33:10 +0300

    Add files via upload

---
+++ FarmdMod.py
@@ -0,0 +1,33 @@
+# meta developer: @stupid_alien_mods
+
+from .. import loader, utils
+import asyncio
+
+@loader.tds
+class FarmdMod(loader.Module):
+    """Модуль для увеличения 🍆 в @dickupbot"""
+    strings = {"name": "FarmdMod"}
+    
+    def __init__(self):
+        self.running = False 
+
+    async def farmdoncmd(self, message):
+        """Включить автоматическую отправку /dick """
+        if self.running:
+            await message.edit("<b>❎Процесс уже запущен!</b>")
+            return
+
+        self.running = True
+        await message.edit("<b>✅Автоматическая отправка /dick запущена!</b>")
+
+        while self.running:
+            await message.client.send_message("@dickupbot", "/dick")
+            await asyncio.sleep(3600)
+    async def farmdoffcmd(self, message):
+        """Отключить автоматическую отправку /dick """
+        if not self.running:
+            await message.edit("<b>❎Процесс не запущен!</b>")
+            return
+
+        self.running = False
+        await message.edit("<b>❎Автоматическая отправка /dick остановлена!</b>")
